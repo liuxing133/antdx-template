b566f527d2fc249cee77ae95db227bf945dcf3bb
liuxing133 <1937526117@qq.com>
2025-03-06 14:48:08 +0800

    first commit

---
+++ src/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Bubble } from '@ant-design/x';
+import Demo from '../Demo';
+
+const Home: React.FC = () => (
+  <div >
+    <Demo/>
+  </div>
+);
+
+export default Home;
